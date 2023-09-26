d25c341cac5b2ffd45255820a935f08c2f1c2710
Nikita Safronov <sns@orbisystems.ru>
2023-09-26 16:31:28 +0300

    [add]: ЗИ ЛР4 - код

---
+++ data-security/lab4/el_gamal.py
@@ -0,0 +1,62 @@
+import argparse
+import math
+
+
+def fast_pow(x, y):
+    if y == 0:
+        return 1
+    if y == -1:
+        return 1. / x
+    p = fast_pow(x, y // 2)
+    p *= p
+    if y % 2:
+        p *= x
+    return p
+
+
+def reverse_element(f, d):
+    X = [1, 0, f]
+    Y = [0, 1, d]
+    while True:
+        if Y[2] == 0:
+            print("Error")
+            return
+        elif Y[2] == 1:
+            return Y[1]
+        else:
+            q = X[2] // Y[2]
+            t = [0, 0, 0]
+            for i in range(0, 3):
+                t[i] = X[i] - q * Y[i]
+                X[i] = Y[i]
+                Y[i] = t[i]
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p")
+    parser.add_argument("-g")
+    parser.add_argument("-x")
+    parser.add_argument("-k")
+    parser.add_argument("-m")
+    args = parser.parse_args()
+
+    p = int(args.p)
+    g = int(args.g)
+    x = int(args.x)
+    k = int(args.k)
+    m = int(args.m)
+    y = math.pow(g, x) % p
+    a = math.pow(g, k) % p
+    f = p - 1
+
+    print(f"Секретные параметры: {x=}, {k=}")
+    print(f"Хэш сообщения: {m=}")
+
+    kr = reverse_element(f, k)
+    b = (kr * (m - x * a)) % f
+
+    if ((fast_pow(y, a) * fast_pow(a, b)) % p) == (fast_pow(g, m) % p):
+        print("Проверка подлинности прошла успешно")
+    else:
+        print("Проверка подлинности завершилась неудачей")
---
+++ data-security/lab4/rsa.py
@@ -0,0 +1,42 @@
+import argparse
+
+
+def fast_pow(x, y):
+    if y == 0:
+        return 1
+    if y == -1:
+        return 1. / x
+    p = fast_pow(x, y // 2)
+    p *= p
+    if y % 2:
+        p *= x
+    return p
+
+
+def encode(message, e, n):
+    return fast_pow(message, e) % n
+
+
+def decode(message, d, n):
+    return fast_pow(message, d) % n
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-n")
+    parser.add_argument("-e")
+    parser.add_argument("-m")
+    parser.add_argument("-s")
+    args = parser.parse_args()
+
+    n = int(args.n)
+    e = int(args.e)
+    s = int(args.s)
+    m = int(args.m)
+
+    print(f"Параметры: {n=}, {e=}")
+    print(f"Сообщение: ({m}, {s})")
+    if encode(m, e, n) == s:
+        print(f"Проверка подлинности для ({m}, {s}) прошла успешно")
+    else:
+        print(f"Проверка подлинности для ({m}, {s}) завершилась неудачей")
