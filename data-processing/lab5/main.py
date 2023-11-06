7055ce67aeccf5b2c9af93ef30d932920fa29516
Nikita Safronov <sns@orbisystems.ru>
2023-11-06 22:38:47 +0300

    [fix]: МОИ - ПЗ5

--- data-processing/lab5/main.py
+++ data-processing/lab5/main.py
@@ -1,54 +1,87 @@
 import argparse
 import csv
-import numpy as np
-from scipy import stats
+
 import matplotlib.pyplot as plt
+import numpy as np
 from prettytable import PrettyTable
+from scipy.stats import norm
 
 
+def get_test_power(
+        data: list[float], theta_0: float, alpha: float, theta: float,
+        sample_size: int
+) -> float:
+    critical_value = norm.ppf(1 - alpha)
+    standard_error = np.std(data) / np.sqrt(sample_size)
+    critical_region = (
+        theta_0 - critical_value * standard_error,
+        theta_0 + critical_value * standard_error
+    )
+    power = 1 - norm.cdf(critical_region[1], theta, standard_error) + norm.cdf(
+        critical_region[0], theta, standard_error
+    )
+    return power
 
-def read_points(path: str) -> list[str]:
+
+def read_points(path: str) -> list[float]:
     points = []
+
     with open(path, newline='') as file:
         reader = csv.reader(file, delimiter=' ', quotechar='|')
         for row in reader:
             points.append(float(''.join(row)))
-    
+
     return points
+
 
 def solve(points: list[float]):
     alpha = 0.1
-    sample_size = len(points)
+    theta_0 = np.mean(points)
+    delta = np.std(points) / np.sqrt(len(points))
+    thetas = [theta_0 + i * delta for i in range(1, 6)]
 
-    sample_mean = np.mean(points)
-    sample_std = np.std(points)
-
-    null_hypothesis_mean = sample_mean 
-    t_statistics =  stats.ttest_1samp(points, sample_mean)
-
-    p_value = 2 * (1 - stats.t.cdf(np.abs(t_statistics), df=sample_size - 1))
-    
-    alternative_hypothesis_means = np.linspace(null_hypothesis_mean, null_hypothesis_mean + 5, 5)
-
-    power_values = np.array([
-        1 - stats.t.cdf((null_hypothesis_mean - alt_mean) / (sample_std / np.sqrt(sample_size)), df=sample_size - 1)
-        for alt_mean in alternative_hypothesis_means
-    ])
-
-    plt.figure()
-    plt.plot(alternative_hypothesis_means, power_values, color='k', label='Мощность критерия')
-    plt.xlabel('Значение параметра распределения')
-    plt.ylabel('Мощность критерия')
-    plt.axhline(alpha, color='r', label=f'$\\alpha = {alpha}$')
-    plt.ylim(0, 1.1)
-    plt.legend()
-    plt.show()
+    small_sample_power = np.array(
+        [
+            get_test_power(
+                points, theta_0, alpha, theta, 25
+            ) for theta in thetas
+        ]
+    )
+    full_sample_power = np.array(
+        [
+            get_test_power(
+                points, theta_0, alpha, theta, len(points)
+            ) for theta in thetas
+        ]
+    )
 
     table = PrettyTable()
-    table.add_column('Значение параметра распределения', alternative_hypothesis_means)
-    table.add_column('Мощность критерия', power_values)
-    table.add_column('Ошибка II рода', 1 - power_values)
+    table.add_column('Значение параметра распределения', thetas)
+    table.add_column('Мощность критерия', small_sample_power)
+    table.add_column('Ошибка II рода', 1 - small_sample_power)
+    print('Любые 25 из заданной выборки:')
     print(table)
+
+    table = PrettyTable()
+    table.add_column('Значение параметра распределения', thetas)
+    table.add_column('Мощность критерия', full_sample_power)
+    table.add_column('Ошибка II рода', 1 - full_sample_power)
+    print('Все значения выборки:')
+    print(table)
+
+    plt.figure(figsize=(10, 5))
+    plt.plot(thetas, small_sample_power, label='Мощность критерия при $N=25$')
+    plt.plot(
+        thetas, full_sample_power, label=f'Мощность критерия $N={len(points)}$'
+    )
+    plt.xlabel('Значение параметра распределения')
+    plt.ylabel('Мощность критерия')
+    plt.axhline(
+        alpha, color='red',
+        label=f"$\\alpha={alpha}$"
+    )
+    plt.legend()
+    plt.show()
 
 
 if __name__ == '__main__':
@@ -59,9 +92,4 @@ if __name__ == '__main__':
     file = args.file or './data/Test14.csv'
 
     points = read_points(file)
-    print('Полный объём исходных данных:')
-    solve(points)
-
-    points = np.random.choice(points, 25)
-    print('Любые 25 значений из заданной выборки:')
     solve(points)
